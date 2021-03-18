8527fe3fb06a84f1db204931ead5bd86a0513d6a
Didier Bröska <didier.broska@gmail.com>
2021-03-18 11:01:23 +0100

    move constantes in top of package

---
+++ depthmap_generator/__init__.py
@@ -0,0 +1,13 @@
+"""
+DepthMap package
+"""
+
+import os
+
+DEFAULT_MODEL_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "model",
+    "model.pt",
+)
+
+DEFAULT_FILE_ID = "1VuJRqB79JJ8GLCJJjbWPmvxGuADMcnVx"
